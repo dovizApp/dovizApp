43dfbbb35dd37593fa3c77c22acef6bdff9f36cc
demetdirik <83430664+demetdirik@users.noreply.github.com>
2023-12-26 18:56:50 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,33 +1,55 @@
-import Login from './screens/Login';
-import SignUp from './screens/SignUp';
-import ToDo from './screens/ToDo';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import React, {Component} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-const Stack = createNativeStackNavigator(); 
-
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
+  let [isLoading, setIsLoading] = useState(true);
+  let [error, setError] = useState();
+  let [response, setResponse] = useState();
+
+  useEffect(() => {
+    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setIsLoading(false);
+        setResponse(result);
+      },
+      (error) => {
+        setIsLoading(false);
+        setError(error);
+      }
+    )
+  }, [] );
+
+  const getContent = () => {
+    if (isLoading){
+      return <ActivityIndicator size="large" />;
+    }
+
+     if (error){
+      return <Text>{error}</Text>
+    }
+    console.log(response);
+    return <Text>Bitcoin (USD) :{response && response.bpi && response.bpi.USD && response.bpi.USD.rate}</Text> ;
+
+   
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{headerShown: false}} />
-        <Stack.Screen
-          name="SignUp"
-          component={SignUp}
-          options={{headerShown: false}} />
-        <Stack.Screen
-          name="ToDo"
-          component={ToDo}
-          options={{headerShown: false}} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      {getContent()}
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
