6968eb63edb8e5146a847fcd006aebf1b29db2e9
demetdirik <83430664+demetdirik@users.noreply.github.com>
2023-12-29 19:05:45 +0300

    api güncellemesi
    apiden veriler çekildi

--- App.js
+++ App.js
@@ -1,83 +1,46 @@
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import xml2js from 'react-native-xml2js';
 
-import 'firebase/auth';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import Login from './screens/Login';
-import Register from './screens/Register';
-
-import app from './firebaseConfig'; // firebaseConfig dosyanızın bulunduğu yola göre düzenleyin
-
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-      </Stack.Navigator>
-    </NavigationContainer>
-=======
-import React, {Component} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
-
-export default function App() {
-  let [isLoading, setIsLoading] = useState(true);
-  let [error, setError] = useState();
-  let [response, setResponse] = useState();
+const ExchangeRates = () => {
+  const [rates, setRates] = useState(null);
 
   useEffect(() => {
-    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
-    .then(res => res.json())
-    .then(
-      (result) => {
-        setIsLoading(false);
-        setResponse(result);
-      },
-      (error) => {
-        setIsLoading(false);
-        setError(error);
+    const fetchExchangeRates = async () => {
+      try {
+        const response = await fetch('https://www.tcmb.gov.tr/kurlar/today.xml');
+        const xmlData = await response.text();
+
+        // XML verisini JavaScript nesnelerine dönüştürme
+        xml2js.parseString(xmlData, (err, result) => {
+          if (err) {
+            console.error('XML parse hatası:', err);
+            return;
+          }
+
+          // Döviz kurları JavaScript nesneleri olarak result objesinde bulunabilir
+          const currencyRates = result.Tarih_Date.Currency;
+          setRates(currencyRates);
+        });
+      } catch (error) {
+        console.error('Veri çekme hatası:', error);
       }
-    )
-  }, [] );
+    };
 
-  const getContent = () => {
-    if (isLoading){
-      return <ActivityIndicator size="large" />;
-    }
-
-     if (error){
-      return <Text>{error}</Text>
-    }
-    console.log(response);
-    return <Text>Bitcoin (USD) :{response && response.bpi && response.bpi.USD && response.bpi.USD.rate}</Text> ;
-
-   
-  };
+    fetchExchangeRates();
+  }, []);
 
   return (
-    <View style={styles.container}>
-      {getContent()}
-      <StatusBar style="auto" />
+    <View>
+      <Text>Döviz Kurları:</Text>
+      {rates &&
+        rates.map((rate, index) => (
+          <Text key={index}>
+            {rate.$.Kod}: {rate.ForexSelling.map((selling) => selling)}
+          </Text>
+        ))}
     </View>
->>>>>>> 43dfbbb35dd37593fa3c77c22acef6bdff9f36cc
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default ExchangeRates;
